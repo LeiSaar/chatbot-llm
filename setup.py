b0da1cfd40ba2367f9e135bc492da09c5f22fe1a
Lei <s8lehuan@stud.uni-saarland.de>
2026-07-17 20:09:04 +0200

    add requirements

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="HRChatbotLLM",
+    version="0.1.0",
+    author="Lei Huang",
+    author_email="2507340599@qq.com",
+    packages=find_packages(),
+    install_requires=[]
+)
